c6bc577f2aa2d93433f38b74b9804eaa59b77e0a
Kiendz3202 <manhkien3202@gmail.com>
2022-10-17 22:43:49 +0700

    first commit

--- contracts/MultiSend.sol
+++ contracts/MultiSend.sol
@@ -2,11 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/security/Pausable.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
-
 interface Token {
     function transfer(address recipient, uint256 amount)
         external
@@ -36,18 +31,11 @@ contract MultiSend {
 
     // modifier to check if the caller is owner
     modifier isOwner() {
-        // If the first argument of 'require' evaluates to 'false', execution terminates and all
-        // changes to the state and to Ether balances are reverted.
-        // This used to consume all gas in old EVM versions, but not anymore.
-        // It is often a good idea to use 'require' to check if functions are called correctly.
-        // As a second argument, you can also provide an explanation about what went wrong.
         require(msg.sender == owner, "Caller is not owner");
         _;
     }
 
-    /**
-     * @dev Set contract deployer as owner
-     */
+    //Set contract deployer as owner
     constructor(Token _tokenAddress) {
         require(
             address(_tokenAddress) != address(0),
@@ -65,10 +53,7 @@ contract MultiSend {
         owner = newOwner;
     }
 
-    /**
-     * @dev Return owner address
-     * @return address of owner
-     */
+    // @dev Return owner address
     function getOwner() external view returns (address) {
         return owner;
     }
